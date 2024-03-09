23fc117a1345833365e08bce060b150b62705fd4
tracer-ics2022 <tracer@njuics.org>
2024-03-09 11:43:11 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:11 up  1:52,  1 user,  load average: 0.18, 0.18, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -14,6 +14,11 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define max(a, b) ((a) > (b) ? (a) : (b))
+bool division_zero = false;
+
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -21,7 +26,8 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, 
+  TK_EQ,
   TK_DECIMAL,  // 十进制整数
   /* TODO: Add more token types */
 
@@ -155,27 +161,123 @@ static bool make_token(char *e) {
   return true;
 }
 
+bool check_parentheses(int p, int q)
+{
+    if(tokens[p].type != '('  || tokens[q].type != ')')
+        return false;
+    int l = p , r = q;
+    while(l < r)
+    {
+        if(tokens[l].type == '('){
+            if(tokens[r].type == ')')
+            {
+                l ++ , r --;
+                continue;
+            }
+
+            else
+                r --;
+        }
+        else if(tokens[l].type == ')')
+            return false;
+        else l ++;
+    }
+    return true;
+}
+ 
+
+uint32_t eval(int p, int q) {
+    if (p > q) {
+        /* Bad expression */
+        assert(0);
+        return -1;
+    }
+    else if (p == q) {
+        /* Single token.
+         * For now this token should be a number.
+         * Return the value of the number.
+         */
+        return atoi(tokens[p].str);
+    }
+    else if (check_parentheses(p, q) == true) {
+        /* The expression is surrounded by a matched pair of parentheses.
+         * If that is the case, just throw away the parentheses.
+         */
+        // printf("check p = %d, q = %d\n",p + 1 , q - 1);
+        return eval(p + 1, q - 1);
+    }
+    /* else if(check_parentheses(p, q) == false){
+       printf("Unique\n");
+       return -1;
+       }
+       */
+    else {
+        int op = -1; // op = the position of 主运算符 in the token expression;
+        bool flag = false;
+        for(int i = p ; i <= q ; i ++)
+        {
+            if(tokens[i].type == '(')
+            {
+                while(tokens[i].type != ')')
+                    i ++;
+            }
+            if(!flag && tokens[i].type == TK_EQ){
+                flag = true;
+                op = max(op,i);
+            }
+            if(!flag && (tokens[i].type == '+' || tokens[i].type == '-')){
+                flag = true;
+                op = max(op, i);
+            }
+            if(!flag && (tokens[i].type == '*' || tokens[i].type == '/') ){
+                op = max(op, i);
+            }
+        }
+        //      printf("op position is %d\n", op);
+        // if register return $register
+        int  op_type = tokens[op].type;
+
+        // 递归处理剩余的部分
+        uint32_t  val1 = eval(p, op - 1);
+        uint32_t  val2 = eval(op + 1, q);
+        //      printf("val1 = %d, val2 = %d \n", val1, val2);
+
+        switch (op_type) {
+            case '+':
+                return val1 + val2;
+            case '-':
+                return val1 - val2;
+            case '*':
+                return val1 * val2;
+            case '/':
+                if(val2 == 0){//printf("division can't zero;\n");
+                    division_zero = true;
+                    return 0;
+                }
+                return val1 / val2;
+            default:
+                printf("No Op type.");
+                assert(0);
+        }
+    }
+}
+
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-  
-    int result = atoi(tokens[0].str);
-    for (int i = 1; i < nr_token; i += 2) {
-        int num = atoi(tokens[i + 1].str);
-        if (tokens[i].type == '+') {
-            result += num;
-        } else if (tokens[i].type == '-') {
-            result -= num;
-        } else if (tokens[i].type == '*') {
-            result *= num;
-        } else if (tokens[i].type == '/') {
-            result /= num;
-        }
+      // 调用eval函数计算表达式的值
+    word_t result = eval(0, nr_token - 1);
+    
+    if (division_zero) {
+        // 处理除以0的情况
+        *success = false;
+        return 0;
     }
+    
+    *success = true;
     return result;
-
-  return 0;
 }
+ 
