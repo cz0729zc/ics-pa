ac1e9a38f37682bb83a20cddde82f735acc90986
tracer-ics2022 <tracer@njuics.org>
2024-10-07 18:27:30 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:27:30 up 12:26,  1 user,  load average: 0.66, 0.43, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -389,6 +389,7 @@ static bool make_token(char *e) {
                     tokens[k - 1] = tokens[k];
                 }
                     tokens_len -- ;
+                    nr_token--;
                 }
             }
 		}
@@ -496,7 +497,7 @@ uint32_t eval(int p, int q) {
         // 递归处理剩余的部分
         uint32_t  val1 = eval(p, op - 1);
         uint32_t  val2 = eval(op + 1, q);
-             printf("val1 = %d, val2 = %d \n", val1, val2);
+             //printf("val1 = %d, val2 = %d \n", val1, val2);
 
         switch (op_type) {
             case '+':
