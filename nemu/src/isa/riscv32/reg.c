01236aa78b505372e498af5f5f3f154e0b66990c
tracer-ics2022 <tracer@njuics.org>
2024-10-07 10:34:19 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:19 up  4:32,  1 user,  load average: 0.30, 0.31, 0.33

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ const char *regs[] = {
 void isa_reg_display() {
     int i;
     for (i = 0; i < sizeof(regs) / sizeof(regs[0]); i++) {
-        printf("%s: 0x%08x\n", regs[i], cpu.gpr[i]);
+        printf("%s: 0x%x\n", regs[i], cpu.gpr[i]);
     }
 }
 
