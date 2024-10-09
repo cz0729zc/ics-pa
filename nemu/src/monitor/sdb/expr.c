cf6d113996b76cb700fc77b93cb02ac44e7c382f
tracer-ics2022 <tracer@njuics.org>
2024-10-09 10:59:09 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:09 up  3:33,  1 user,  load average: 0.17, 0.25, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -469,14 +469,13 @@ uint32_t eval(int p, int q) {
          printf("check p = %d, q = %d\n",p , q);
         return eval(p + 1, q - 1);
     }
-    /* else if(check_parentheses(p, q) == false){
+    else if(check_parentheses(p, q) == false){
        printf("Unique\n");
        return -1;
        }
-       */
     else {
         int op = -1; // op = the position of 主运算符 in the token expression;
-        int min_priority = 100; // 初始化为一个较大的值
+        int min_priority = 1000; // 初始化为一个较大的值
         int parentheses_level = 0; // 记录当前括号的层级
 
         for (int i = p; i <= q; i++) {
