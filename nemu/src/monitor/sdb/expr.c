3052556c42011ec0848d39aac31859e0b35a8311
tracer-ics2022 <tracer@njuics.org>
2024-10-09 10:34:12 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:12 up  3:09,  1 user,  load average: 0.43, 0.44, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -462,7 +462,7 @@ uint32_t eval(int p, int q) {
         /* The expression is surrounded by a matched pair of parentheses.
          * If that is the case, just throw away the parentheses.
          */
-         printf("check p = %d, q = %d\n",p + 1 , q - 1);
+         printf("check p = %d, q = %d\n",p , q);
         return eval(p + 1, q - 1);
     }
     /* else if(check_parentheses(p, q) == false){
