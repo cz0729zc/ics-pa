adfd9777c364536a96faee0157545f07921409c5
tracer-ics2022 <tracer@njuics.org>
2024-10-13 22:07:30 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:07:30 up  3:41,  1 user,  load average: 1.27, 1.02, 0.67

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -64,8 +64,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_J:                   immJ(); break;
     //case TYPE_B: src1R(); src2R(); immB(); printf("rs1: %u,rs2: %u, rd: %u, imm: %d\n",*src1,*src2,*rd,*imm); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
-    case TYPE_R: src1R(); src2R(); printf("rs1: %3d,  rs2: %3d, rd: %3u\n",*src1,*src2,*rd);       ; break;
-    //case TYPE_R: src1R(); src2R();       ; break;
+    //case TYPE_R: src1R(); src2R(); printf("rs1: %3d,  rs2: %3d, rd: %3u\n",*src1,*src2,*rd);       ; break;
+    case TYPE_R: src1R(); src2R();       ; break;
   }
 }
 
