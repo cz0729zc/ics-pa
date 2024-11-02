a9ede5405efaf14035a31cc7f7882f1f2bff32a6
tracer-ics2022 <tracer@njuics.org>
2024-11-02 12:55:48 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:55:48 up  1:10,  1 user,  load average: 1.82, 0.71, 0.40

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -187,17 +187,11 @@ static int cmd_x(char *args){
     sscanf(baseaddr,"%x", &addr);
     printf("addr = 0x%x\n", addr);
     
-
-    uint32_t data = paddr_read(addr, 4);
-    printf("0x%08x: 0x%08x\n", addr, data);
-
-    data = paddr_read(addr + 4, 4);
-    printf("0x%08x: 0x%08x\n", addr + 4, data);
-    // for(int i = len ; i > 0 ; i --)
-    // {
-    //     uint32_t data = paddr_read(addr - i * 4, 4); // 循环读取 4 字节数据  
-    //     printf("0x%08x: 0x%08x\n", addr - i * 4, data); // 使用 %08x 格式化为 8 位（填充零）  
-    // }
+    for(int i = 0 ; i < len ; i++)
+    {
+        uint32_t data = paddr_read(addr + i * 4, 4); // 循环读取 4 字节数据  
+        printf("0x%08x: 0x%08x\n", addr + i * 4, data); // 使用 %08x 格式化为 8 位（填充零）  
+    }
     
     return 0;
 }
