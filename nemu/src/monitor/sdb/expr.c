e24f4dc64d02f5fc9fb7be63ec2f53085ed673cf
tracer-ics2022 <tracer@njuics.org>
2024-11-02 12:26:42 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:26:42 up 41 min,  1 user,  load average: 0.04, 0.10, 0.14

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -379,13 +379,16 @@ static bool make_token(char *e) {
 		    // 获取 '*' 后的数字或地址值，并进行解引用
 		    int tmp = char_int(tokens[i+1].str); // 将数字或地址字符串转为整数
             printf("tmp: %u\n",tmp);
-		    uintptr_t addr = (uintptr_t)tmp;     // 将整数转换为指针地址
-            printf("addr: 0x%08lx\n",addr);
-		    word_t value = *((word_t*)addr); // 解引用该地址
-            printf("value: 0x%08x\n",value);
+
+		    word_t addr = (word_t)tmp;     // 将整数转换为指针地址
+            printf("addr: 0x%08x\n",addr);
+
+		    //word_t value = *((word_t*)addr); // 解引用该地址
+            //printf("value: 0x%08x\n",value);
+
 		    // 将解引用后的值转换为字符串，存储在 tokens[i+1]
-		    unsigned_long_char(value, tokens[i+1].str); // 将值写回 tokens[i+1]
-            printf("value: %s\n",tokens[i+1].str);
+		   //unsigned_long_char(value, tokens[i+1].str); // 将值写回 tokens[i+1]
+            //printf("value: %s\n",tokens[i+1].str);
 
 		    // 删除 `*` 这个 token，并调整数组长度
 		    for (int j = i; j < tokens_len - 1; j++) {
