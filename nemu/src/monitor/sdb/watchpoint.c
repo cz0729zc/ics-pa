dc21016342b726ea777bb6d144779a3e91feee25
tracer-ics2022 <tracer@njuics.org>
2024-11-02 21:48:15 +0800

    >  compile NEMU 202210120324 zhangchi Linux zc-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:48:15 up 10:02,  1 user,  load average: 0.26, 0.24, 0.24

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -112,7 +112,8 @@ bool check_watchpoints() {
     bool any_changed = false;  // 用于标记是否有监视点的值发生变化  
 
     while (current_wp != NULL) {  // 循环条件设置为新建的监视点不为head的下一个监视点  
-        bool success = false;  
+        bool success = false; 
+        printf("urrent_wp->expr: %s",current_wp->expr);
         word_t current_value = expr(current_wp->expr, &success);  // 调用expr()函数计算表达式的值  
         
         if (success) {  
